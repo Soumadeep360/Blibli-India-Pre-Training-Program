cc9dc5756b0c1030fccffbd1a97c070246c0c229
Soumadeep Sarkar <72089988+Soumadeep360@users.noreply.github.com>
2024-11-02 01:54:10 +0530

    Create Java_Static_Initializer_Block.java

---
+++ Backend (JAVA)/HackerRank Solutions/Java_Static_Initializer_Block.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    static int B;
+    static int H;
+    static boolean flag=true;
+    static {
+        Scanner scanner = new Scanner(System.in);
+      
+            B = scanner.nextInt();
+            H = scanner.nextInt();
+            
+            if (B <= 0 || H <= 0) {
+                flag=false;
+            }
+    }
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        if(flag){
+            System.out.println(H*B);
+        }
+        else{
+            System.out.println("java.lang.Exception: Breadth and height must be positive");      
+        }
+    }
+}
