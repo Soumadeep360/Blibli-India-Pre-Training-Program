396917ab953865fa739885e82ed24de03075a1ec
Soumadeep Sarkar <72089988+Soumadeep360@users.noreply.github.com>
2024-11-01 22:31:35 +0530

    Create Java_Stdin_and_Stdout_I.java

---
+++ Backend (JAVA)/HackerRank Solutions/Java_Stdin_and_Stdout_I.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+       Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        System.out.println(a);
+        int b = scanner.nextInt();
+        System.out.println(b);
+        int c = scanner.nextInt();
+        System.out.println(c);
+    }
+}
