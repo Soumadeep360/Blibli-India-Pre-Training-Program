4d86fc05393ab85e5de347a4438e33f3deff8f1e
Soumadeep Sarkar <72089988+Soumadeep360@users.noreply.github.com>
2024-11-02 16:18:30 +0530

    Create Java_String_Tokens.java

---
+++ Backend (JAVA)/HackerRank Solutions/Java_String_Tokens.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+    String s = scan.nextLine();
+
+    s = s.trim();
+    String token[] = s.split("[ !,?._'@]+");
+
+    if(s.length() == 0)
+    {
+        System.out.println(0);
+    }else{
+        System.out.println(token.length);
+    }
+
+    for(String tokens : token)
+    {
+        System.out.println(tokens);
+    }
+    }
+}
+
