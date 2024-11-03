c308924ee7703ac2481cd53a5f5277d697ede37f
Soumadeep Sarkar <72089988+Soumadeep360@users.noreply.github.com>
2024-11-03 20:03:13 +0530

    Create Java_Hashset.java

---
+++ Backend (JAVA)/HackerRank Solutions/Java_Hashset.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Solution {
+
+public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    sc.nextLine();
+    Set<String> set=new HashSet<>();
+    for(int i=0;i<n;i++){
+        String str1=sc.nextLine();
+        set.add(str1);
+       System.out.println(set.size());  
+    }
+
+}
+}
