ea25ae983f7a4a3b848855f31347c1dc04b9a38b
Soumadeep Sarkar <72089988+Soumadeep360@users.noreply.github.com>
2024-11-04 15:24:18 +0530

    Create Java_Interface.java

---
+++ Backend (JAVA)/HackerRank Solutions/Java_Interface.java
@@ -0,0 +1,27 @@
+import java.util.*;
+interface AdvancedArithmetic {
+    int divisor_sum(int n);
+}
+
+class MyCalculator implements AdvancedArithmetic {
+    public int divisor_sum(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                sum += i;
+            }
+        }
+        return sum;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        MyCalculator my_calculator = new MyCalculator();
+        System.out.println("I implemented: " + AdvancedArithmetic.class.getSimpleName());
+        int sum = my_calculator.divisor_sum(n);
+        System.out.println(sum);
+    }
+}
