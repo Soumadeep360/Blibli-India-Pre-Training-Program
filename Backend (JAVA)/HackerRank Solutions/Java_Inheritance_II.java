24ca4771e74ef20de780874371d20fbf2d028c6e
Soumadeep Sarkar <72089988+Soumadeep360@users.noreply.github.com>
2024-11-04 15:13:12 +0530

    Create Java_Inheritance_II.java

---
+++ Backend (JAVA)/HackerRank Solutions/Java_Inheritance_II.java
@@ -0,0 +1,18 @@
+class Arithmetic {
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
+
+class Adder extends Arithmetic {
+    // Inherits the add method from Arithmetic
+}
+
+public class Solution {
+    public static void main(String[] args) {
+        Adder adder = new Adder();
+        System.out.println("My superclass is: " + Arithmetic.class.getSimpleName());
+        
+        System.out.print(adder.add(42, 0)+" "+adder.add(13, 0)+" "+adder.add(20, 0));
+    }
+}
