c2edc2abb8fc23f318f45edf956bef60790b0843
Soumadeep360 <soumadeepsarkar4@gmail.com>
2024-11-13 12:12:27 +0530

    new code added

---
+++ Frontend(HTML-CSS-JS)/Basic JavaScript/nesting-for-loops.js
@@ -0,0 +1,11 @@
+function multiplyAll(arr) {
+  let ans = 1;
+  for (let i = 0; i < arr.length; i ++){
+    for (let j = 0; j < arr[i].length; j ++){
+      ans *= arr[i][j];
+    }
+  }
+  return ans;
+}
+
+multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
